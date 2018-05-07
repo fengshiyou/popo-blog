cdc0067f6b1a503f1881ccb2d03efc1cf94a81b4
fengshiyou <501453944@qq.com>
2018-05-07 18:27:41 +0800

    用户登陆和注册

--- src/components/blog/BlogListBody.js
+++ src/components/blog/BlogListBody.js
@@ -6,11 +6,18 @@ import {getHeightByRadio} from '../../until/Tool'
 
 export default class BlogListBody extends React.Component {
     render() {
+        let search = '';
+        if(this.props.location.pathname == '/myblog'){//我的博客
+            search = "?uid=" + localStorage.getItem('uid');
+        }else{//查询博客
+            search = this.props.location.search;
+        }
+
         return (
             <div style={{minHeight: getHeightByRadio()}} className="dark-gray-back">
                 <Row>
                     <Col span={16} className="white-back margin-l-50 margin-t-50">
-                        <BlogList search={this.props.location.search}/>
+                        <BlogList search={search}/>
                     </Col>
                     <Col span={5} className="white-back margin-l-50 margin-t-50">
                         <div>标签和目录导航</div>
